83697a53d230d8ad6bc70f62b5042eab02bda1e6
LuckyNoobz <r.abudzar@gmail.com>
2024-03-08 17:21:24 +0700

    Reactive data

--- chapter 1/app.js
+++ chapter 1/app.js
@@ -1,8 +1,20 @@
+var product = {
+    "name": "Bamboo thermal ski coat",
+    "description": "This is a great ski coat for all you bamboo lovers out there. Made from 100% organic bamboo, it's lightweight and warm",
+    "price": "99"
+    // "image": ""
+}
+
 var app = new Vue({
     el: "#app",
-    data: {
-      name: "John Doe",
-      age: 32,
-      email: "john@gmail.com",
-    },
-  });
+    data: product
+})
+
+// var app = new Vue({
+//     el: "#app",
+//     data: {
+//       name: "John Doe",
+//       age: 32,
+//       email: "john@gmail.com",
+//     },
+//   });
